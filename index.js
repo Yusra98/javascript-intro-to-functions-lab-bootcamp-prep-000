a45084a0810ab81f544669923ab2be2d9c0cd765
Yusra Faki <yusrafaki7@gmail.com>
2018-07-26 14:02:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   return string.toLowerCase()
   if ( var lowercase = 'hello!' lowercase.toLowerCase() === lowercase) {
-    return string()
+    return 
   }
 }
